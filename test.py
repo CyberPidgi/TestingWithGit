3aab4817e92bd5d82b3562020629731782cea4cd
PigeonCoder2 <allengeroge79@gmail.com>
2022-08-28 10:56:48 +0400

    ez commit

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 def testfn():
     print("Hello World Sucks.")
     print("i am so confused about commiting bro.")
+    print("NOW THIS WORKS????")
 
 def newfn():
     print("Surely this should not conflict.")
